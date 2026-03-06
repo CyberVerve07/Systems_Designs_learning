3ce7d0ad695c371cc7ef41a38fa06642292af4c1
CyberVerve07 <adityamahajan7287@gmail.com>
2026-03-06 22:36:41 +0530

    Updated code/load_balancer/

---
+++ code/load_balancer/basic_example/Main.java
@@ -0,0 +1,7 @@
+package systemdesign.load_balancer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello System Design Learning - Load Balancer");
+    }
+}
